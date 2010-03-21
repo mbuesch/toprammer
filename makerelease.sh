c3bade5124f84ccb0114eedd20c61fa765a10e86
Michael Buesch <mb@bu3sch.de>
2010-03-21 19:56:10 +0100

    Do a testbuild in the release script
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- makerelease.sh
+++ makerelease.sh
@@ -6,6 +6,9 @@ project="toprammer"
 
 origin="$PWD/$(dirname $0)"
 
+do_git_tag=1
+[ "$1" = "--notag" ] && do_git_tag=0
+
 version_major="$(cat $origin/libtoprammer/toprammer_main.py | grep -e VERSION_MAJOR | head -n1 | cut -d'=' -f2)"
 version_minor="$(cat $origin/libtoprammer/toprammer_main.py | grep -e VERSION_MINOR | head -n1 | cut -d'=' -f2)"
 version="$(printf %d.%d $version_major $version_minor)"
@@ -13,6 +16,7 @@ if [ -z "$version" ]; then
 	echo "Could not determine version!"
 	exit 1
 fi
+tmpdir="/tmp"
 release_name="$project-$version"
 tarball="$release_name.tar.bz2"
 tagname="release-$version"
@@ -20,25 +24,33 @@ tagmsg="$project-$version release"
 
 export GIT_DIR="$origin/.git"
 
-cd /tmp/
+cd "$tmpdir"
 rm -Rf "$release_name"
-echo "Creating target directory"
+echo "Creating target directory..."
 mkdir "$release_name"
 cd "$release_name"
 echo "git checkout"
 git checkout -f
-
 rm makerelease.sh
 
-echo "creating tarball"
-cd ..
+echo "Creating tarball..."
+cd "$tmpdir"
 tar cjf "$tarball" "$release_name"
 mv "$tarball" "$origin"
+
+echo "Running testbuild..."
+cd "$tmpdir/$release_name"
+./setup.py build
+
+echo "Cleaning up..."
+cd "$tmpdir"
 rm -Rf "$release_name"
 
-echo "Tagging GIT"
-cd "$origin"
-git tag -m "$tagmsg" -a "$tagname"
+if [ "$do_git_tag" -ne 0 ]; then
+	echo "Tagging GIT"
+	cd "$origin"
+	git tag -m "$tagmsg" -a "$tagname"
+fi
 
 echo
-echo "built release $version"
+echo "Built release $tarball"
