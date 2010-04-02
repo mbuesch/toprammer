bb27a9a05c32d7c7295a457e57b610f7291baeaa
Michael Buesch <mb@bu3sch.de>
2010-04-02 15:08:15 +0000

    attiny13: Put SDO high to 0x10
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- libtoprammer/bit/src/attiny13dip8/attiny13dip8.v
+++ libtoprammer/bit/src/attiny13dip8/attiny13dip8.v
@@ -152,7 +152,8 @@ module attiny13dip8(data, ale, write, read, osc_in, zif);
 
 	always @(negedge read) begin
 		case (address)
-		8'h10: begin /* Unused */
+		8'h10: begin /* Get SDO sequence high (bits 3-10) */
+			read_data[7:0] <= sdo_buf[10:3];
 		end
 		8'h12: begin /* Read status */
 			read_data[0] <= (prog_busy[0] != prog_busy[1]);	/* busy */
@@ -160,9 +161,6 @@ module attiny13dip8(data, ale, write, read, osc_in, zif);
 		end
 		8'h13: begin /* Get SDO sequence low (bits 0-7) */
 			read_data[7:0] <= sdo_buf[7:0];
-		end
-		8'h14: begin /* Get SDO sequence high (bits 3-10) */
-			read_data[7:0] <= sdo_buf[10:3];
 		end
 		endcase
 	end
