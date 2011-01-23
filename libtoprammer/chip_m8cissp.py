1a4cad80b17bc625b947cb9a2ea810a19d4134d6
Michael Buesch <mb@bu3sch.de>
2011-01-23 20:19:28 +0100

    Add m8c stuff
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- libtoprammer/chip_m8cissp.py
+++ libtoprammer/chip_m8cissp.py
@@ -3,7 +3,7 @@
 #
 #    Cypress M8C In System Serial Programmer
 #
-#    Copyright (c) 2010 Michael Buesch <mb@bu3sch.de>
+#    Copyright (c) 2010-2011 Michael Buesch <mb@bu3sch.de>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -34,6 +34,7 @@ class Chip_M8C_ISSP(Chip):
 	STAT_BUSY1		= 0x02
 	STAT_ISSPSTATE		= 0x1C
 	STAT_ISSPSTATE_SHIFT	= 2
+	STAT_SDATA		= 0x20
 
 	STRVEC_SETFREQ = {
 		2	: "1001111110000000101000",
@@ -141,6 +142,7 @@ class Chip_M8C_ISSP(Chip):
 		"1101111111100010010000",
 	)
 	STRVEC_READBYTE = "101aaaaaaaaZDDDDDDDDZ0"
+	STRVEC_WRITEBYTE = "100aaaaaaaadddddddd000"
 	STRVEC_ERASEALL = (
 		"1101111011100000000000",
 		"1101111011001000000000",
@@ -169,23 +171,101 @@ class Chip_M8C_ISSP(Chip):
 		"1101111100000000000000",
 		"1101111111100010010000",
 	)
+	STRVEC_WRITESECBYTE = "10010aaaaaadddddddd000"
+	STRVEC_SETBLKNUM = "10011111010dddddddd000"
+	STRVEC_READBLK = (
+		"1101111011100000000000",
+		"1101111011000001000000",
+		"1001111100000111010000",
+		"1001111100100001011000",
+		"1101111010100000000000",
+		"1101111010000000011000",
+		"1001111101100100000000",
+		"1101111100100110000000",
+		"1101111101001000000000",
+		"1101111000000000001000",
+		"1101111100000000000000",
+		"1101111111100010010000",
+	)
+	STRVEC_WRITEBLK = (
+		"1101111011100000000000",
+		"1101111011000001000000",
+		"1001111100000111010000",
+		"1001111100100001011000",
+		"1101111010100000000000",
+		"1101111010000000011000",
+		"1001111101100100000000",
+		"1101111100100110000000",
+		"1101111101001000000000",
+		"1101111000000000010000",
+		"1101111100000000000000",
+		"1101111111100010010000",
+	)
+	STRVEC_READCHKSUM = "10111111001ZDDDDDDDDZ010111111000ZDDDDDDDDZ0"
 
 	def __init__(self):
 		Chip.__init__(self)
+#		self.progmemSize = 1024 * 16
+		self.progmemSize = 128#FIXME
 
-	def initializeChip(self): #FIXME put somewhere else
-		self.printDebug("Initializing chip")
-		self.top.vccx.setLayoutMask(0)
-		self.top.vpp.setLayoutMask(0)
-		self.top.gnd.setLayoutPins( [] )
-		self.top.cmdSetVCCXVoltage(5)
-		self.top.cmdSetVPPVoltage(0)
-		self.top.cmdSetVPPVoltage(5)
-
-		#XXX
+	def readSignature(self):
+		self.progressMeterInit("Reading chip ID", 0)
 		self.__powerOnReset()
-		id = self.__readID()
-		print "ID=0x%04X" % id
+		gotID = self.__readID()
+		self.progressMeterFinish()
+
+		return int2byte(gotID & 0xFF) + int2byte((gotID >> 8) & 0xFF)
+
+	def erase(self):
+		self.progressMeterInit("Erasing chip", 0)
+		self.__powerOnReset()
+		self.__sendSetFreqVector()
+		for vec in self.STRVEC_ERASEALL:
+			self.__loadStringVector(vec)
+			self.__runCommandSync(self.ISSPCMD_SENDVEC)
+		self.__runCommandSync(self.ISSPCMD_EXEC)
+		self.progressMeterFinish()
+
+	def writeProgmem(self, image):
+		if len(image) > self.progmemSize or len(image) % 64 != 0:
+			self.throwError("Invalid program memory image size %d "
+					"(expected <=%d and multiple of 64)" %\
+				(len(image), self.progmemSize))
+
+		self.progressMeterInit("Writing program memory", len(image))
+		self.__powerOnReset()
+		for blknum in range(0, len(image) // 64):
+			for i in range(0, 64):
+				self.progressMeter(blknum * 64 + i)
+				self.__writeByte(i, byte2int(image[blknum * 64 + i]))
+			self.__sendSetFreqVector()
+			vec = self.__stringVectorReplace(self.STRVEC_SETBLKNUM, "d", blknum)
+			self.__loadStringVector(vec)
+			self.__runCommandSync(self.ISSPCMD_SENDVEC)
+			for vec in self.STRVEC_WRITEBLK:
+				self.__loadStringVector(vec)
+				self.__runCommandSync(self.ISSPCMD_SENDVEC)
+			self.__runCommandSync(self.ISSPCMD_EXEC)
+		self.progressMeterFinish()
+
+	def readProgmem(self):
+		self.progressMeterInit("Reading program memory", self.progmemSize)
+		self.__powerOnReset()
+		assert(self.progmemSize % 64 == 0)
+		image = []
+		for blknum in range(0, self.progmemSize // 64):
+			vec = self.__stringVectorReplace(self.STRVEC_SETBLKNUM, "d", blknum)
+			self.__loadStringVector(vec)
+			self.__runCommandSync(self.ISSPCMD_SENDVEC)
+			for vec in self.STRVEC_READBLK:
+				self.__loadStringVector(vec)
+				self.__runCommandSync(self.ISSPCMD_SENDVEC)
+			self.__runCommandSync(self.ISSPCMD_EXEC)
+			for i in range(0, 64):
+				self.progressMeter(blknum * 64 + i)
+				image.append(int2byte(self.__readByte(i)))
+		self.progressMeterFinish()
+		return b"".join(image)
 
 	def __powerDown(self):
 		"Turn the power to the device off"
@@ -195,10 +275,15 @@ class Chip_M8C_ISSP(Chip):
 
 	def __powerOnReset(self):
 		"Perform a complete power-on-reset and initialization"
+		self.top.vccx.setLayoutMask(0)
+		self.top.vpp.setLayoutMask(0)
+		self.top.gnd.setLayoutMask(0)
+		self.top.cmdSetVCCXVoltage(5)
+		self.top.cmdSetVPPVoltage(5)
+
 		self.printDebug("Initializing supply power...")
 		self.top.gnd.setLayoutPins( (20,) )
 		self.top.vccx.setLayoutPins( (21,) )
-		self.top.cmdSetVCCXVoltage(5)
 
 		self.__powerDown()
 		self.printDebug("Performing a power-on-reset...")
@@ -208,18 +293,17 @@ class Chip_M8C_ISSP(Chip):
 		for vec in self.STRVEC_INIT1[1:]:
 			self.__loadStringVector(vec)
 			self.__runCommandSync(self.ISSPCMD_SENDVEC)
-		self.printDebug("Executing...")
 		self.__runCommandSync(self.ISSPCMD_EXEC)
 		self.printDebug("Sending vector 2...")
 		for vec in self.STRVEC_INIT2:
 			self.__loadStringVector(vec)
 			self.__runCommandSync(self.ISSPCMD_SENDVEC)
-		self.printDebug("Executing...")
 		self.__runCommandSync(self.ISSPCMD_EXEC)
 		self.printDebug("Sending vector 3...")
 		for vec in self.STRVEC_INIT3_HIVDD:
 			self.__loadStringVector(vec)
 			self.__runCommandSync(self.ISSPCMD_SENDVEC)
+#		self.__runCommandSync(self.ISSPCMD_EXEC)
 
 	def __readID(self):
 		"Read the silicon ID"
@@ -239,6 +323,18 @@ class Chip_M8C_ISSP(Chip):
 		self.__runCommandSync(self.ISSPCMD_SENDVEC)
 		input = self.__getInputVector()
 		return (input >> 2) & 0xFF
+
+	def __writeByte(self, address, byte):
+		vec = self.__stringVectorReplace(self.STRVEC_WRITEBYTE, "a", address)
+		vec = self.__stringVectorReplace(vec, "d", byte)
+		self.__loadStringVector(vec)
+		self.__runCommandSync(self.ISSPCMD_SENDVEC)
+
+	def __sendSetFreqVector(self):
+		sclkFreq = 2	# 2 Mhz. Hardcoded in FPGA.
+		strVec = self.STRVEC_SETFREQ[sclkFreq]
+		self.__loadStringVector(strVec)
+		self.__runCommandSync(self.ISSPCMD_SENDVEC)
 
 	def __loadCommand(self, command):
 		self.top.cmdFPGAWrite(0x12, command & 0xFF)
@@ -278,16 +374,16 @@ class Chip_M8C_ISSP(Chip):
 
 	def __getStatusFlags(self):
 		self.top.cmdFPGARead(0x12)
-		stat = self.top.cmdReadBufferReg()
-		stat = byte2int(stat[0])
+		stat = self.top.cmdReadBufferReg8()
 		isspState = (stat & self.STAT_ISSPSTATE) >> self.STAT_ISSPSTATE_SHIFT
+		sdata = bool(stat & self.STAT_SDATA)
 		isBusy = bool(stat & self.STAT_BUSY0) != bool(stat & self.STAT_BUSY1)
-		self.printDebug("isspState = 0x%02X, isBusy = %d, busyFlags = 0x%01X" %\
-			(isspState, isBusy, (stat & (self.STAT_BUSY0 | self.STAT_BUSY1))))
-		return (isBusy, isspState)
+		self.printDebug("isspState = 0x%02X, isBusy = %d, busyFlags = 0x%01X, sdata = %d" %\
+			(isspState, isBusy, (stat & (self.STAT_BUSY0 | self.STAT_BUSY1)), sdata))
+		return (isBusy, sdata, isspState)
 
 	def __busy(self):
-		(isBusy, isspState) = self.__getStatusFlags()
+		(isBusy, sdata, isspState) = self.__getStatusFlags()
 		return isBusy
 
 	def __busyWait(self):
@@ -301,21 +397,20 @@ class Chip_M8C_ISSP(Chip):
 		self.top.cmdFPGARead(0x13)
 		self.top.cmdFPGARead(0x14)
 		self.top.cmdFPGARead(0x15)
-		stat = self.top.cmdReadBufferReg()
-		return byte2int(stat[0]) | (byte2int(stat[1]) << 8) | (byte2int(stat[2]) << 16)
+		return self.top.cmdReadBufferReg24()
 
 	def __stringVectorToBinary(self, vector):
 		binary = 0
-		input = 0
+		inputMask = 0
 		assert(len(vector) == 22)
 		bit = len(vector) - 1
 		for b in vector:
 			if b == "1":
 				binary |= (1 << bit)
 			if b == "H" or b == "L" or b == "Z" or b == "D":
-				input |= (1 << bit)
+				inputMask |= (1 << bit)
 			bit -= 1
-		return (binary, input)
+		return (binary, inputMask)
 
 	def __stringVectorReplace(self, strVec, replace, data):
 		ret = ""
@@ -333,6 +428,7 @@ class Chip_M8C_ISSP(Chip):
 
 	def __loadStringVector(self, strVec):
 		(vector, inputMask) = self.__stringVectorToBinary(strVec)
+#		print "Loading vector 0x%06X, 0x%06X" % (vector, inputMask)
 		self.__loadVectorInputMask(inputMask)
 		self.__loadVector(vector)
 
