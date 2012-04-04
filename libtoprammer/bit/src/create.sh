37fa1a303fb719f1c33970babfe6b3f31bce5fa8
Michael Buesch <m@bues.ch>
2012-04-04 16:21:17 +0200

    create.sh: Port to dash
    Signed-off-by: Michael Buesch <m@bues.ch>

--- libtoprammer/bit/src/create.sh
+++ libtoprammer/bit/src/create.sh
@@ -1,34 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 # Create source template
-# Copyright (c) 2010 Michael Buesch <m@bues.ch>
+# Copyright (c) 2010-2012 Michael Buesch <m@bues.ch>
 # Licensed under the GNU/GPL v2+
 
 basedir="$(dirname "$0")"
-[ "${basedir:0:1}" = "/" ] || basedir="$PWD/$basedir"
+[ "$(echo -n "$basedir" | cut -c1)" = "/" ] || basedir="$PWD/$basedir"
 
 template="$basedir/template"
 
 set -e
 
-function usage
+usage()
 {
 	echo "Usage: create.sh TARGET_NAME"
 }
 
-if [ $# -ne 1 ]; then
+[ $# -eq 1 ] || {
 	usage
 	exit 1
-fi
+}
 name="$1"
 
 target="$basedir/$name"
 
 mkdir -p "$target"
-for file in $(ls $template); do
-	suffix="$(echo $file | cut -d. -f2)"
+for file in $(ls "$template"); do
+	suffix="$(echo "$file" | cut -d. -f2)"
 	targetfile="$name.$suffix"
-	[ $file != Makefile ] || targetfile=$file
-	cat $template/$file | sed -e 's/template/'$name'/' >$target/$targetfile
+	[ "$file" = "Makefile" ] && targetfile="$file"
+	cat "$template/$file" |\
+		sed -e 's/template/'"$name"'/' > "$target/$targetfile"
 done
 
 exit 0
