b52180d9483a0c7e1b0fb90426633905d4332d7a
Michael Buesch <m@bues.ch>
2012-04-28 13:48:48 +0200

    hm62256 bottom half: Port to generic bottom half framework
    Signed-off-by: Michael Buesch <m@bues.ch>

--- libtoprammer/fpga/src/hm62256dip28/hm62256dip28.v
+++ libtoprammer/fpga/src/hm62256dip28/hm62256dip28.v
@@ -4,7 +4,7 @@
  *   HM62256 SRAM
  *   FPGA bottomhalf implementation
  *
- *   Copyright (c) 2011 Michael Buesch <m@bues.ch>
+ *   Copyright (c) 2011-2012 Michael Buesch <m@bues.ch>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,88 +21,53 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/* The runtime ID and revision. */
-`define RUNTIME_ID	16'h000A
-`define RUNTIME_REV	16'h01
+`include "common.vh"
 
-module hm62256dip28(data, ale, write, read, zif);
-	inout [7:0] data;
-	input ale;
-	input write;
-	input read;
-	inout [48:1] zif;
-
-	/* Interface to the microcontroller */
-	wire read_oe;		/* Read output-enable */
-	reg [7:0] address;	/* Cached address value */
-	reg [7:0] read_data;	/* Cached read data */
-
-	/* Chip (DUT) signals */
+`BOTTOMHALF_BEGIN(hm62256dip28, 10, 1)
 	reg [14:0] dut_addr;
 	reg [7:0] dut_data;
 	reg dut_ce;
 	reg dut_oe;
 	reg dut_we;
 
-	wire low, high;		/* Constant lo/hi */
+	`ASYNCPROC_BEGIN
+		/* Nothing to do */
+	`ASYNCPROC_END
 
-	assign low = 0;
-	assign high = 1;
-
-	always @(posedge write) begin
-		case (address)
-		8'h10: begin /* Bulk write */
-			dut_data <= data;
+	`DATAWRITE_BEGIN
+		`ADDR(0): begin /* Bulk write */
+			dut_data <= in_data;
 		end
-		8'h11: begin /* /CE, /OE, /WE */
-			dut_ce <= data[0];
-			dut_oe <= data[1];
-			dut_we <= data[2];
+		`ADDR(1): begin /* /CE, /OE, /WE */
+			dut_ce <= in_data[0];
+			dut_oe <= in_data[1];
+			dut_we <= in_data[2];
 		end
-		8'h12: begin /* Addr byte 0 */
-			dut_addr[7:0] <= data[7:0];
+		`ADDR(2): begin /* Addr byte 0 */
+			dut_addr[7:0] <= in_data[7:0];
 		end
-		8'h13: begin /* Addr byte 1 */
-			dut_addr[14:8] <= data[6:0];
+		`ADDR(3): begin /* Addr byte 1 */
+			dut_addr[14:8] <= in_data[6:0];
 		end
-		endcase
-	end
+	`DATAWRITE_END
 
-	always @(negedge read) begin
-		case (address)
-		8'h10: begin /* Bulk read */
-			read_data[0] <= zif[21];
-			read_data[1] <= zif[22];
-			read_data[2] <= zif[23];
-			read_data[3] <= zif[25];
-			read_data[4] <= zif[26];
-			read_data[5] <= zif[27];
-			read_data[6] <= zif[28];
-			read_data[7] <= zif[29];
+	`DATAREAD_BEGIN
+		`ADDR(0): begin /* Bulk read */
+			out_data[0] <= zif[21];
+			out_data[1] <= zif[22];
+			out_data[2] <= zif[23];
+			out_data[3] <= zif[25];
+			out_data[4] <= zif[26];
+			out_data[5] <= zif[27];
+			out_data[6] <= zif[28];
+			out_data[7] <= zif[29];
 		end
+	`DATAREAD_END
 
-		8'hFD: read_data <= `RUNTIME_ID & 16'hFF;
-		8'hFE: read_data <= (`RUNTIME_ID >> 8) & 16'hFF;
-		8'hFF: read_data <= `RUNTIME_REV;
-		endcase
-	end
-
-	always @(negedge ale) begin
-		address <= data;
-	end
-
-	assign read_oe = !read && address[4];
-
-	bufif0(zif[1], low, low);
-	bufif0(zif[2], low, low);
-	bufif0(zif[3], low, low);
-	bufif0(zif[4], low, low);
-	bufif0(zif[5], low, low);
-	bufif0(zif[6], low, low);
-	bufif0(zif[7], low, low);
-	bufif0(zif[8], low, low);
-	bufif0(zif[9], low, low);
-	bufif0(zif[10], low, low);
+	`ZIF_UNUSED(1)	`ZIF_UNUSED(2)	`ZIF_UNUSED(3)
+	`ZIF_UNUSED(4)	`ZIF_UNUSED(5)	`ZIF_UNUSED(6)
+	`ZIF_UNUSED(7)	`ZIF_UNUSED(8)	`ZIF_UNUSED(9)
+	`ZIF_UNUSED(10)
 	bufif0(zif[11], dut_addr[14], low);	/* A14 */
 	bufif0(zif[12], dut_addr[12], low);	/* A12 */
 	bufif0(zif[13], dut_addr[7], low);	/* A7 */
@@ -131,23 +96,8 @@ module hm62256dip28(data, ale, write, read, zif);
 	bufif0(zif[36], dut_addr[13], low);	/* A13 */
 	bufif0(zif[37], dut_we, low);		/* /WE */
 	bufif0(zif[38], high, low);		/* VCC */
-	bufif0(zif[39], low, low);
-	bufif0(zif[40], low, low);
-	bufif0(zif[41], low, low);
-	bufif0(zif[42], low, low);
-	bufif0(zif[43], low, low);
-	bufif0(zif[44], low, low);
-	bufif0(zif[45], low, low);
-	bufif0(zif[46], low, low);
-	bufif0(zif[47], low, low);
-	bufif0(zif[48], low, low);
-
-	bufif1(data[0], read_data[0], read_oe);
-	bufif1(data[1], read_data[1], read_oe);
-	bufif1(data[2], read_data[2], read_oe);
-	bufif1(data[3], read_data[3], read_oe);
-	bufif1(data[4], read_data[4], read_oe);
-	bufif1(data[5], read_data[5], read_oe);
-	bufif1(data[6], read_data[6], read_oe);
-	bufif1(data[7], read_data[7], read_oe);
-endmodule
+	`ZIF_UNUSED(39)	`ZIF_UNUSED(40)	`ZIF_UNUSED(41)
+	`ZIF_UNUSED(42)	`ZIF_UNUSED(43)	`ZIF_UNUSED(44)
+	`ZIF_UNUSED(45)	`ZIF_UNUSED(46)	`ZIF_UNUSED(47)
+	`ZIF_UNUSED(48)
+`BOTTOMHALF_END
