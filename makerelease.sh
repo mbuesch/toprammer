7eb0eac5438d11df4f379a406ce1c5183b85abc0
Michael Buesch <m@bues.ch>
2012-05-04 19:22:43 +0200

    Rewrite release script
    Signed-off-by: Michael Buesch <m@bues.ch>

--- makerelease.sh
+++ makerelease.sh
@@ -1,57 +1,29 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-project="toprammer"
+srcdir="$(dirname "$0")"
+[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
 
+srcdir="$srcdir" # git repos root
 
-origin="$(dirname "$0")"
-[ "${origin:0:1}" = "/" ] || origin="$PWD/$origin"
+die() { echo "$*"; exit 1; }
 
-do_git_tag=1
-[ "$1" = "--notag" ] && do_git_tag=0
+# Import the makerelease.lib
+# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+for path in $(echo "$PATH" | tr ':' ' '); do
+	[ -f "$MAKERELEASE_LIB" ] && break
+	MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
 
-version_major="$(cat $origin/libtoprammer/main.py | grep -e VERSION_MAJOR | head -n1 | cut -d'=' -f2)"
-version_minor="$(cat $origin/libtoprammer/main.py | grep -e VERSION_MINOR | head -n1 | cut -d'=' -f2)"
-if [ -z "$version_major" -o -z "$version_minor" ]; then
-	echo "Could not determine version!"
-	exit 1
-fi
-version="$(printf %d.%d $version_major $version_minor)"
-tmpdir="/tmp"
-release_name="$project-$version"
-tarball="$release_name.tar.bz2"
-tagname="release-$version"
-tagmsg="$project-$version release"
+hook_get_version()
+{
+	local file="$1/libtoprammer/main.py"
+	local major="$(cat "$file" | grep -e VERSION_MAJOR | head -n1 | cut -d'=' -f2)"
+	local minor="$(cat "$file" | grep -e VERSION_MINOR | head -n1 | cut -d'=' -f2)"
+	[ -n "$major" -a -n "$minor" ] ||\
+		die "Failed to get version"
+	version="$(printf '%d.%d' "$major" "$minor")"
+}
 
-export GIT_DIR="$origin/.git"
-
-cd "$tmpdir"
-rm -Rf "$release_name"
-echo "Creating target directory..."
-mkdir "$release_name"
-cd "$release_name"
-echo "git checkout"
-git checkout -f
-rm makerelease.sh
-
-echo "Creating tarball..."
-cd "$tmpdir"
-tar cjf "$tarball" "$release_name"
-mv "$tarball" "$origin"
-
-echo "Running testbuild..."
-cd "$tmpdir/$release_name"
-./setup.py build
-
-echo "Cleaning up..."
-cd "$tmpdir"
-rm -Rf "$release_name"
-
-if [ "$do_git_tag" -ne 0 ]; then
-	echo "Tagging GIT"
-	cd "$origin"
-	git tag -m "$tagmsg" -a "$tagname"
-fi
-
-echo
-echo "Built release $tarball"
+project=toprammer
+makerelease "$@"
